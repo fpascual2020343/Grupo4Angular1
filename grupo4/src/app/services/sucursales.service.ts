c58099924fc2bd91dc6ad6415c69ec83b0b7b176
Hugo Daniel Velásquez Patzan <88805843+hvelasquez2020242@users.noreply.github.com>
2022-04-22 11:17:17 -0600

    Agregando el crud de sucursales

---
+++ grupo4/src/app/services/sucursales.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { Sucursales } from 'src/app/models/sucursales.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class SucursalesService{
+
+  public url : String = 'http://localhost:3000/api'
+  constructor(public _http: HttpClient) { }
+  public headersVariable = new HttpHeaders().set('Content-Type', 'application/json')
+
+  obtenerSucursales(token) : Observable<any> {
+    let headersToken = this.headersVariable.set('Authorization', token )
+
+
+    return this._http.get(this.url + '/obtenerSucursales', {headers: headersToken});
+
+
+  }
+  agregarSucursales(modeloSucursales: Sucursales, token) : Observable<any> {
+    let headersToken = this.headersVariable.set('Authorization', token )
+
+    let parametros = JSON.stringify(modeloSucursales);
+
+    return this._http.post(this.url + '/agregarSucursal', parametros, { headers: headersToken});
+  }
+  editarSucursales(modeloSucursales: Sucursales, token) : Observable<any> {
+    let headersToken = this.headersVariable.set('Authorization', token )
+
+    let parametro = JSON.stringify(modeloSucursales);
+
+    return this._http.put(this.url + '/editarSucursal/' + modeloSucursales._id, parametro, {headers: headersToken})
+  }
+
+
+  obtenerSucursalId( idSucursal, token ): Observable<any> {
+    let headersToken = this.headersVariable.set('Authorization', token )
+
+
+    return this._http.get(this.url + '/obtenerSucursal/' + idSucursal, { headers: headersToken})
+  }
+
+}
